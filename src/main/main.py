5db28f0168c5b34baf7f40e47252f7b941dbaf4e
Zephren <zephren123@gmail.com>
2021-07-29 23:04:09 -0700

    new directories

---
+++ src/main/main.py
@@ -0,0 +1,12 @@
+## Author: Zephren de la Cerda
+
+## take in theater name
+## call omdb, scrape theater site, gather list of movies in box office that day
+
+func gettheatername()
+
+
+## web scrape movies from box office page
+
+theaterLink = https://www.regmovies.com/movies
+func getregalboxofficemovies()
